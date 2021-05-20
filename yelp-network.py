b2713bd4d1c97b832b6bb138cba76febe95aa7cc
razor999920 <43019476+razor999920@users.noreply.github.com>
2021-05-19 22:00:03 -0400

    c started

--- yelp-network.py
+++ yelp-network.py
@@ -4,57 +4,28 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 def drawEdges(data_file):
-    # Graph
-    G = nx.Graph()
 
     with open(data_file, 'r', encoding='utf8') as business:
         yelp_data = csv.reader(business, delimiter=",")
-
-        spliting_friends = []
-
     
         for row in yelp_data:
             
             if row[4] != 'None':
-                # Split the friends     
-                # spliting_friends = row[4].split(",")
-                
+      
                 spliting_friends = row[4].split(",")
+                # To add the data in TXT file
+                social_network = open('yelp-network.txt', 'a')
 
                 for friend in spliting_friends:
-                    if ((friend, row[0]) not in spliting_friends):
-                        spliting_friends.append((row[0], friend))
+                    social_network.write(row[0] + ' ' + friend + "\n")
 
-                # To add the data in TXT file
-                # social_network = open('yelp-network.txt', 'a')
-                # social_network.close()
+                social_network.close()
 
-                # print(spliting_friends)
-
-                # Add user
-                # for friend in spliting_friends:
-                #     #  Add User frined
-                #     if friend not in G.nodes():
-                #         G.add_node(friend)
-                    
-                #     G.add_edge(row[0], friend)
-
-
-                    # social_network.write(row[0] + ' ' + str(friend) + "\n") 
-    G = nx.Graph()
-
-    for user in spliting_friends:
-        print(user)
-        # G.add_node(user[0])
-        # G.add_node(user[1])
-        # G.add_edge(user[0], user[1])
-    
-    # social_network = open('yelp-network.txt', 'a')
-
-    # for edge in G.edges():
-    # #    social_network.write(edge[0] + ' ' + edge[1] + "\n")
-    #     print(edge)
-
+    with open ('yelp-network.txt', 'r', encoding='utf8') as network:
+        social_data = csv.reader(network, delimiter=",")
+        
+        # for row in social_data:
+        print(social_data)
 
 def main():
 
@@ -70,7 +41,6 @@ def main():
     # print(G.edges())
     # nx.draw(G)
     # plt.show()
-
 
     # Arugment Validations
     if (len(sys.argv) == 0):
